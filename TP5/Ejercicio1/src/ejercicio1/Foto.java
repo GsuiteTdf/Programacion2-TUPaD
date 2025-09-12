45c111ba633b1b6dfa2b8ca6c0825d89075422b6
GsuiteTdf <gaston.cejas@gmail.com>
2025-09-12 16:53:17 -0300

    Add files via upload

---
+++ TP5/Ejercicio1/src/ejercicio1/Foto.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author GastonCejas
+ */
+public class Foto {
+    private String imagen;
+    private String formato;
+    
+
+    public Foto(String imagen, String formato) {
+        this.imagen = imagen;
+        this.formato = formato;
+    }
+
+    // Getters y Setters
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getFormato() {
+        return formato;
+    }
+
+    public void setFormato(String formato) {
+        this.formato = formato;
+    }
+
+    @Override
+    public String toString() {
+        return "Foto{" + "imagen=" + imagen + ", formato=" + formato + '}';
+    }    
+}
