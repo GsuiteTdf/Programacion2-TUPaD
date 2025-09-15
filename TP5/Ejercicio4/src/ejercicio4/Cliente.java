ff233adf8bae49daa6bf22b9bbdfa1d270446b88
GsuiteTdf <gaston.cejas@gmail.com>
2025-09-15 13:04:33 -0300

    Add files via upload

---
+++ TP5/Ejercicio4/src/ejercicio4/Cliente.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author GastonCejas
+ */
+public class Cliente {
+	private String nombre;
+	private String dni;
+	private TarjetaDeCredito tarjeta;
+
+	public Cliente(String nombre, String dni) {
+		this.nombre = nombre;
+		this.dni = dni;
+		this.tarjeta = null;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public TarjetaDeCredito getTarjeta() {
+		return tarjeta;
+	}
+
+	// Asociación bidireccional
+	public void setTarjeta(TarjetaDeCredito tarjeta) {
+		this.tarjeta = tarjeta;
+		if (tarjeta != null && tarjeta.getTitular() != this) {
+			tarjeta.setTitular(this);
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente{" +
+				"nombre='" + nombre + '\'' +
+				", dni='" + dni + '\'' +
+				", tarjeta=" + (tarjeta != null ? tarjeta.getNumero() : "null") +
+				'}';
+	}
+}
