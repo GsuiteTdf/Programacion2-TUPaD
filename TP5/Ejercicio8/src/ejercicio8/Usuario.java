492d9f061346a92ecc36d16eb562fb56bad81748
GsuiteTdf <gaston.cejas@gmail.com>
2025-09-17 11:10:28 -0300

    Add files via upload

---
+++ TP5/Ejercicio8/src/ejercicio8/Usuario.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio8;
+
+
+/**
+ *
+ * @author GastonCejas
+ */
+public class Usuario {
+	private String nombre;
+	private String email;
+
+	public Usuario(String nombre, String email) {
+		this.nombre = nombre;
+		this.email = email;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public String toString() {
+        return "Usuario{" + "nombre=" + nombre + ", email=" + email + '}';
+    }
+}
