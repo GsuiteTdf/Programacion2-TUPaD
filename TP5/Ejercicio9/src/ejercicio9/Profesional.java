0d278459994f42332bda3b182c06cebb96e157fa
GsuiteTdf <gaston.cejas@gmail.com>
2025-09-17 11:24:00 -0300

    Add files via upload

---
+++ TP5/Ejercicio9/src/ejercicio9/Profesional.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio9;
+
+
+/**
+ *
+ * @author GastonCejas
+ */
+public class Profesional {
+	private String nombre;
+	private String especialidad;
+
+	public Profesional(String nombre, String especialidad) {
+		this.nombre = nombre;
+		this.especialidad = especialidad;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+    public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getEspecialidad() {
+		return especialidad;
+	}
+
+	public void setEspecialidad(String especialidad) {
+		this.especialidad = especialidad;
+	}
+
+    /**
+     *
+     * @return
+     */
+    @Override
+	public String toString() {
+		return "Profesional{" +
+				"nombre='" + nombre + '\'' +
+				", especialidad='" + especialidad + '\'' +
+				'}';
+	}
+}
